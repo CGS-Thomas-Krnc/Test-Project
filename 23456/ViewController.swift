d6c5b71d61e4cb362c9a49af6651f17df29829d1
krncy <1642@cgs.act.edu.au>
2015-07-23 11:41:55 +1000

    Inital commit

---
+++ 23456/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  23456
+//
+//  Created by Thomas Krnc on 23/07/2015.
+//  Copyright (c) 2015 Thomas Krnc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
